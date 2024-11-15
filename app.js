a60ed582da732519c2f347460472b2ef0fc3551f
Alexandre Sumbane <alexandresumbane7@gmail.com>
2024-11-15 12:38:15 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+
+export default app;
+
+
