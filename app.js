e0d38422294f2a9f1816d3d91b403138d7514891
Alexandre Sumbane <alexandresumbane7@gmail.com>
2024-11-25 11:41:30 +0200

    success and cancel

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import Stripe from 'stripe';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,10 +15,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
+//Home Route
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
+
+
+//Stripe
+//let stripeGateway = Stripe(process.env.stripe_api_key)
+
 
 
 export default app;
